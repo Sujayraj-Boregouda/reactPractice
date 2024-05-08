d471276ad898244da9b9f917e174811c7ec3203a
Sujayraj Boregouda <sujayraj.gouda@gmail.com>
2024-05-08 22:25:27 +0530

    Revert "First Commit"
    This reverts commit 1f05f82ea4cc7255a549a5980a3a37b5d19f3ed9.

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = ({disabled, className, onClick, children}) => {
-    return (
-        <button className={className} onClick={onClick} disabled={disabled}>{children}</button>
-    )
-}
-
-export default Button;
